9d4ac8985e0e1509e76810c4c00ed24df203b9e9
foschieraanderson <foschieraanderson@gmail.com>
2020-08-31 15:55:51 -0300

    Resolução desafio do Modulo 2

--- Modulo-2/Desafio/main.py
+++ Modulo-2/Desafio/main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractclassmethod
 
 
 class Department:
@@ -6,33 +7,50 @@ class Department:
         self.code = code
 
 
-class Employee:
-    def __init__(self, code, name, salary):
+class Employee(ABC):
+
+    WORKLOAD = 8
+    PERCENTAGE_BONUS = 0.15
+
+    def __init__(self, code, name, salary, department):
         self.code = code
         self.name = name
         self.salary = salary
+        self.__department = department
 
+    @abstractclassmethod
     def calc_bonus(self):
         pass
 
-    def get_hours(self):
-        pass
+    @classmethod
+    def get_hours(cls):
+        return cls.WORKLOAD
+
+    def get_department(self):
+        return self.__department.name
+
+    def set_department(self, departament_name):
+        self.__department.name = departament_name
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
+        super().__init__(code, name, salary, Department('managers', 1))
 
     def calc_bonus(self):
-        return self.salary * 0.15
+        return self.salary * Employee.PERCENTAGE_BONUS
 
 
-class Seller(Manager):
+class Seller(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.sales = 0
+        super().__init__(code, name, salary, Department('sellers', 2))
+        self.__sales = 0
 
-    def get_hours(self):
-        return 6
+    def calc_bonus(self):
+        return self.__sales * Employee.PERCENTAGE_BONUS
+
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, sale):
+        self.__sales += sale
