b18dc2bbe4cd33d04b9e4f11524b7c0db3963d6f
foschieraanderson <foschieraanderson@gmail.com>
2020-09-03 14:11:30 -0300

    Resolução do desafio modulo 5

--- Modulo-5/Desafio/api/models.py
+++ Modulo-5/Desafio/api/models.py
@@ -1,21 +1,43 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinLengthValidator
+
+
+LEVEL_CHOICES = [
+    ('CRITICAL', 'CRITICAL'),
+    ('DEBUG', 'DEBUG'),
+    ('WARNING', 'WARNING'),
+    ('INFO', 'INFO'),
+]
 
 
 class User(models.Model):
-    raise NotImplementedError
-
+  name = models.CharField(max_length=50)
+  last_login = models.DateTimeField(auto_now_add=True)
+  email = models.EmailField(validators=[EmailValidator()])
+  password = models.CharField(max_length=50,
+                              validators=[MinLengthValidator(8)])
 
 class Agent(models.Model):
-    raise NotImplementedError
+  name = models.CharField(max_length=50)
+  status = models.BooleanField()
+  env = models.CharField(max_length=20)
+  version = models.CharField(max_length=5)
+  address = models.GenericIPAddressField(protocol='IPv4')
 
 
 class Event(models.Model):
-    raise NotImplementedError
+  level = models.CharField(max_length=20, choices=LEVEL_CHOICES)
+  data = models.TextField()
+  arquivado = models.BooleanField()
+  date = models.DateField(auto_now_add=True)
+  agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Group(models.Model):
-    raise NotImplementedError
+  name = models.CharField(max_length=50)
 
 
 class GroupUser(models.Model):
-    raise NotImplementedError
+  group = models.ForeignKey(Group, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
